f184d465814dffbead807b6ec04680d990cda01f
ForzenString <forzen-string@git.com>
2023-03-05 20:52:02 +0800

    feat(state) 添加曲线救国，获取可变操作

--- lib/todo_item.dart
+++ lib/todo_item.dart
@@ -17,13 +17,14 @@ class TodoItemTile extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() => TodoItemTileState();
-
-  set record(TodoRecord record) {
-    this.record = record;
-  }
 }
 
 class TodoItemTileState extends State<TodoItemTile> {
+  late TodoRecord record;
+  TodoItemTileState() {
+    record = widget.record;
+  }
+
   /// 要求实现以下功能
   ///
   /// UI 显示参考 原型
@@ -33,7 +34,8 @@ class TodoItemTileState extends State<TodoItemTile> {
   /// 3. 点击本身时，会展开自身和全部group, 如果有group 全部完成了，可以不展开
   ///
   /// ## 注意
-  /// 1. 如果record 中的group 长度为 0 那就是一个只有标题的todo Item, 使用 [ListTile](https://www.geeksforgeeks.org/listtile-widget-in-flutter/)
+  /// 1. 如果record 中的group 长度为 0 那就是一个只有标题的todo Item,
+  /// 使用 [ListTile](https://www.geeksforgeeks.org/listtile-widget-in-flutter/)
   /// 2. 如果 record 中group 长度为 1 且 group.title 为 null, 表示为只有单级的todo Item, 展开内容直接使用 SingleTodoItem 不使用 TodoGroup,
   /// 3. 如果 record 中 group 长度 > 1 且每一个 group title 均非 null, 那就是带分组的`Todo Item`，展开内容使用 TdooGroup
   /// 4. 其他情况全部为异常情况，需要抛出异常
@@ -52,6 +54,7 @@ class TodoItemTileState extends State<TodoItemTile> {
   /// 7. 优先级 显示在 title 内部，红底白字，使用 Expanded 和正文划分空间
   /// 8. tags 在 `subtitle` 中，需要自动适应屏幕宽度换行 （Wrap）
   /// 9. 正文最大行数需要小于3行
+  ///
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
